5e3df6ce7504d6f1ac5a13e5e32b7ee1dc1f3cf3
nako-ay <a.y.1125.ami@gmail.com>
2025-08-11 17:55:21 +0900

    ⚫︎以下の要件を実装
    ・2つ目の画面では1つ目の画面で入力された名前を使って、「こんにちは、名前さん」と表示してください
    ・2つ目の画面でUIButtonを押せば、1つ目の画面へ戻れるようにしてください

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let displayNameViewController:displayNameViewController = segue.destination as! displayNameViewController
+        
+        displayNameViewController.nameText=nameText.text!
+        
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
+   
 
 
 }
--- SecondKadaiApp/displayNameViewController.swift
+++ SecondKadaiApp/displayNameViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class displayNameViewController: UIViewController {
 
+    @IBOutlet weak var name: UILabel!
+    
+    var nameText:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        name.text = "こんにちは、 \(nameText) さん"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
